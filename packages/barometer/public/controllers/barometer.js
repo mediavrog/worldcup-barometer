367983564656ad806e26c2874c871de6d6b6a4d2
Maik Vlcek <maik@mediavrog.net>
2014-06-21 14:45:47 +0900

    implemented cheering directive which works for teams only as well as teams in a match / registered watcher in barometer controller to react to model changes and update the cheering bar / implemented polling on barometer to retrieve changes in cheering from database

--- packages/barometer/public/controllers/barometer.js
+++ packages/barometer/public/controllers/barometer.js
@@ -1,15 +1,15 @@
 'use strict';
 
-angular.module('mean.barometer').controller('BarometerController', ['$scope', 'Global', 'Barometer', 'Matches',
-    function ($scope, Global, Barometer, Matches) {
+angular.module('mean.barometer').controller('BarometerController', ['$scope', '$interval', 'Global', 'Barometer', 'Matches',
+    function ($scope, $interval, Global, Barometer, Matches) {
         $scope.global = Global;
-        $scope.match = {};
+        $scope.match = null;
         $scope.matches = [];
         $scope.team_support = [
             {
                 percentage: 50,
                 value: 0,
-                type: 'success'
+                type: 'warning'
             },
             {
                 percentage: 50,
@@ -18,27 +18,66 @@ angular.module('mean.barometer').controller('BarometerController', ['$scope', 'G
             }
         ];
 
-        $scope.selectMatch = function (match) {
-            $scope.match = match;
-
-            var team1Support = $scope.match.team1.support;
-            var team2Support = $scope.match.team2.support;
-            var team1SupportPercentage = 50, team2SupportPercentage = 50;
-            if (team1Support > 0 && team2Support > 0) {
-                team1SupportPercentage = team1Support * 100 / (team1Support + team2Support);
-                team2SupportPercentage = 100 - team1SupportPercentage;
-            }
-            $scope.team_support[0].value = team1Support;
-            $scope.team_support[0].percentage = team1SupportPercentage;
-            $scope.team_support[1].value = team2Support;
-            $scope.team_support[1].percentage = team2SupportPercentage;
-        };
-
-        $scope.loadRelevantMatch = function () {
+        /**
+         * Initializes this controller with matches for the dropdown and
+         * sets up the most relevant (upcoming) match for display.
+         */
+        var poller;
+        $scope.initialize = function () {
             Matches.most_relevant(function (matches) {
                 $scope.matches = matches;
-                $scope.selectMatch(matches[0]);
+                $scope.match = matches[0];
+
+                // checks for updates match support values
+                // TODO: use server push (e. g. socket.io for braodcasting on model updates)
+                poller = $interval(function () {
+                    $scope.loadMatch($scope.match._id);
+                }, 10000);
             });
         };
+
+        /**
+         * Reloads the currently selected match
+         */
+        $scope.loadMatch = function (matchId) {
+            Matches.get({
+                matchId: matchId
+            }, function (match) {
+                console.log('Loaded match..', match);
+                $scope.match = match;
+            });
+        };
+
+        /**
+         * Cleans up the update timer (polling)
+         */
+        $scope.stopUpdates = function () {
+            if (angular.isDefined(poller)) {
+                $interval.cancel(poller);
+                poller = undefined;
+            }
+        };
+
+        $scope.$on('$destroy', function () {
+            // Make sure that the interval is destroyed
+            $scope.stopUpdates();
+        });
+
+        $scope.$watch('match', function (newMatch, oldMatch) {
+            console.log('Match  was updated ', newMatch);
+            if (newMatch !== null) {
+                var team1Support = $scope.match.team1_support;
+                var team2Support = $scope.match.team2_support;
+                var team1SupportPercentage = 50, team2SupportPercentage = 50;
+                if (team1Support > 0 && team2Support > 0) {
+                    team1SupportPercentage = team1Support * 100 / (team1Support + team2Support);
+                    team2SupportPercentage = 100 - team1SupportPercentage;
+                }
+                $scope.team_support[0].value = team1Support;
+                $scope.team_support[0].percentage = team1SupportPercentage;
+                $scope.team_support[1].value = team2Support;
+                $scope.team_support[1].percentage = team2SupportPercentage;
+            }
+        }, true); // performs equality match
     }
 ]);
--- packages/barometer/public/directives/teams.js
+++ packages/barometer/public/directives/teams.js
@@ -1,14 +1,70 @@
 'use strict';
 
-angular.module('mean.barometer').directive('teamInfo', function () {
-    return {
-        scope: {
-            ngModel: '='
-        },
-        restrict: 'E',
-        replace: true,
-        template: '<div class="teamInfo"><img height="72" width="72" src="/barometer/assets/img/flags/{{ngModel.slug | uppercase}}.png"/>' +
-            '<h2>{{ngModel.title}}</h2>' +
-            '<a href="https://twitter.com/search?src=typd&q=%23{{ngModel.slug}}">#{{ngModel.slug}}</a></div>'
-    };
-});
+angular.module('mean.barometer')
+    .directive('teamInfo', function () {
+        return {
+            scope: {
+                ngModel: '='
+            },
+            restrict: 'E',
+            replace: true,
+            template: '<div class="teamInfo"><img height="72" width="72" ng-src="/barometer/assets/img/flags/{{ngModel.slug | uppercase}}.png"/>' +
+                '<h2>{{ngModel.title}}</h2>' +
+                '<a href="https://twitter.com/search?src=typd&q=%23{{ngModel.slug}}">#{{ngModel.slug}}</a></div>'
+        };
+    })
+    .directive('actionButtonCheer', ['$http', function ($http) { //http://stackoverflow.com/a/22142363/472262
+        return {
+            scope: {
+                ngTeam: '=',
+                ngMatch: '='
+            },
+            restrict: 'E',
+            replace: true,
+            template: '<div class="btn btn-success center-block">' +
+                '<img height="64" width="64" ng-src="/barometer/assets/img/flags/{{ngTeam.slug | uppercase}}.png"/>' +
+                '<br />Cheer for {{ngTeam.title}}' +
+                '</div>',
+            link: function (scope, elem, attrs) {
+                elem.bind('click', function () {
+                    console.log('Cheer for team ' + scope.ngTeam.slug);
+
+                    // match handling
+                    if (angular.isDefined(scope.ngMatch)) {
+                        console.log('... in match ' + scope.ngMatch._id);
+                        if (scope.ngMatch.team1.slug === scope.ngTeam.slug) {
+                            scope.ngMatch.team1_support++;
+                        } else {
+                            scope.ngMatch.team2_support++;
+                        }
+                    }
+
+                    // always update team support
+                    scope.ngTeam.support++;
+                    scope.$apply();
+                });
+            }
+        };
+    }])
+    .directive('actionButtonBoo', ['$http', function ($http) { //http://stackoverflow.com/a/22142363/472262
+        return {
+            scope: {
+                ngModel: '=',
+                matchId: '@'
+            },
+            restrict: 'E',
+            replace: true,
+            template: '<div class="btn btn-warning center-block">Boo</div>{{matchId}}'
+        };
+    }])
+    .directive('twitterStream', ['$http', function ($http) {
+        return {
+            scope: {
+                keyword: '@'
+            },
+            restrict: 'E',
+            replace: true,
+            template: '<div class="teamInfo">{{keyword}}</div>'
+        };
+    }])
+;
